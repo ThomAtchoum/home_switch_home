0d80cfa3e275e5bf1feeaa934f672f1ca98ea1b6
Doge <clement.kubec@isep.fr>
2014-12-09 17:45:58 +0100

    Merge origin/master
    Conflicts:
    	view/form_user.php

--- view/my_profile.php
+++ view/my_profile.php
@@ -1,19 +1,15 @@
 
-<?php
 
-
-
-$req = $DB->prepare('SELECT login, description, rating, picture FROM user WHERE id=:id');
-$req->execute(array('id'=>$_GET['id']));
-
-echo '<ul>';
+<ul>
+    <?php
 while ($donnees = $req->fetch())
 {
 	echo '<li>' . $donnees['login'] . $donnees['description'] . $donnees['rating'] . $donnees['picture'] .'</li>';
 }
-echo '</ul>';
-
-$req->closeCursor();
-
 ?>
+</ul>
+<?php
+$req->closeCursor();
+?>
+
     
