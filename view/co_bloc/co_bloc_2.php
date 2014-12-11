0e9d6ee6a8525104b5728c930ea801da0d22a774
Pierre <pierre.prunieres@live.fr>
2014-12-11 15:22:31 +0100

    Merge origin/master
    Conflicts:
    	Pierre/HouseCard/create_hc_modele.php

--- view/co_bloc/co_bloc_2.php
+++ view/co_bloc/co_bloc_2.php
@@ -1,7 +1,7 @@
 <aside class="champ"> <!--Champs de connexion-->
     <div class="SeCo"> Se connecter </div>
     <div>
-        <form method="post" action="encart.php" class="txtid" onsubmit="return verifForm(f)">
+        <form method="post" action="co_bloc.php" class="txtid" onsubmit="return verifForm(f)">
                 <input type="text" name="login" id="login" maxlength="30" size="17" placeholder="Identifiant" onblur="verifPseudo(this)"/> <!-- Champ identifiant de 30 caractères maxi-->
             <br/>
             <input type="password" name="password" id="password" maxlength="30" size="17" placeholder="Mot de passe" onblur="verifPass(this)"/> <!-- Champ mdp de 30 caractères maxi-->
